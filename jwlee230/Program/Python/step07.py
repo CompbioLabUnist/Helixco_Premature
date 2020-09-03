097810b74836134fc893c0f198eb85224e951396
Jaewoong Lee <230@fumire.moe>
2020-09-03 17:54:10 +0900

    Use everything; add Premature in metadata

--- jwlee230/Program/Python/step07.py
+++ jwlee230/Program/Python/step07.py
@@ -7,9 +7,15 @@ import pandas
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("info", help="Informative XLSX file", type=str, nargs=1)
     parser.add_argument("input", help="Input file (FQ.gz) names", type=str, nargs="+")
 
     args = parser.parse_args()
+
+    if not args.info[0].endswith(".xlsx"):
+        raise ValueError("INFO file must end with .xlsx")
+
+    info_data = pandas.read_excel(args.info[0])
 
     data = pandas.DataFrame()
     data["#SampleID"] = sorted(list(set(list(map(lambda x: x.split("_")[0], list(map(lambda x: x.split("/")[-1], args.input)))))))
@@ -18,6 +24,7 @@ if __name__ == "__main__":
     data["Site"] = list(map(lambda x: {"B1": "Baby-1day", "B3": "Baby-3day", "B5": "Baby-5day", "M": "Mouth", "C": "Cervix", "V": "Vagina", "P": "Placenta"}[x.split("-")[-1]], data["#SampleID"]))
     data["Mother"] = list(map(lambda x: x.split("-")[0], data["#SampleID"]))
     data["BabyNumber"] = list(map(lambda x: x.split("-")[1] if len(x.split("-")) == 3 else "1", data["#SampleID"]))
+    data["Premature"] = list(map(lambda x: "Premature" if int(x) < 37 else "Normal", list(map(lambda x: list(info_data.loc[(info_data["바코드"].str.startswith(x))]["분만주수"])[0].split("+")[0], data["Mother"]))))
     data["Description"] = ""
 
     print("\t".join(data.columns))
---
+++ jwlee230/Program/Python/step10-2.py
@@ -0,0 +1,31 @@
+"""
+step10-2.py: select everything and make pandas
+"""
+import argparse
+import pandas
+import step00
+
+
+def read_raw_data(path: str) -> pandas.DataFrame:
+    """
+    read_raw_data: Read & clearify data from raw TSV
+    """
+    if not path.endswith(".tsv"):
+        raise ValueError("Input is not TSV file")
+
+    data = pandas.read_csv(path, sep="\t", skiprows=1)
+    data.set_index(inplace=True, keys=["taxonomy", "#OTU ID"], verify_integrity=True)
+    data = data.T
+
+    return data
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="Raw TSV file", type=str, nargs=1)
+    parser.add_argument("output", help="Output TAR.gz file", type=str, nargs=1)
+
+    args = parser.parse_args()
+
+    step00.make_pickle(args.output[0], read_raw_data(args.input[0]))
