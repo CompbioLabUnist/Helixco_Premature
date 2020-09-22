03957f4647e44608140240afdf18249f28ee4101
Jaewoong Lee <230@fumire.moe>
2020-09-22 22:41:45 +0900

    Do everything with RandomForest

--- jwlee230/Program/Python/step26.py
+++ jwlee230/Program/Python/step26.py
@@ -2,12 +2,15 @@
 step26.py: RandomForest Classifier
 """
 import argparse
+import tarfile
+import typing
 import matplotlib
 import matplotlib.pyplot
 import seaborn
 import sklearn.ensemble
 import sklearn.manifold
 import sklearn.model_selection
+import sklearn.tree
 import pandas
 import step00
 
@@ -23,9 +26,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.cpu < 1:
-        raise ValueError("CPU must be greater than zero")
+        raise ValueError("CPU must be greater than zero!!")
     elif not args.meta[0].endswith(".tsv"):
-        raise ValueError("Metadata file must end with .TSV")
+        raise ValueError("Metadata file must end with .TSV!!")
+    elif not args.output[0].endswith(".tar"):
+        raise ValueError("Output file must end with .tar!!")
+
+    tar_files: typing.List[str] = list()
 
     train_data = step00.read_pickle(args.train[0])
     normal_data = step00.read_pickle(args.normal[0])
@@ -35,27 +42,67 @@ if __name__ == "__main__":
     train_data = train_data[intersect_columns]
     normal_data = normal_data[intersect_columns]
 
-    tsne_data = pandas.DataFrame(sklearn.manifold.TSNE(n_components=2, init="pca", random_state=0, method="exact", n_jobs=args.cpu).fit_transform(pandas.concat([train_data, normal_data], verify_integrity=True)), columns=["TSNE1", "TSNE2"])
-    for column in tsne_data.columns:
-        tsne_data[column] = sklearn.preprocessing.scale(tsne_data[column])
-
     train_data["Answer"] = list(metadata["premature"])
     normal_data["Answer"] = "Normal"
 
+    # Get Feature Importances
     classifier = sklearn.ensemble.RandomForestClassifier(criterion="entropy", max_features=None, n_jobs=args.cpu, random_state=0, bootstrap=False)
     classifier.fit(train_data[intersect_columns], train_data["Answer"])
     feature_importances = classifier.feature_importances_
-    best_features = list(map(lambda x: x[1], sorted(zip(feature_importances, intersect_columns), reverse=True)))[:10]
+    best_features = list(map(lambda x: x[1], sorted(list(filter(lambda x: x[0] > 0, zip(feature_importances, intersect_columns))), reverse=True)))
 
-    seaborn.set(context="poster", style="whitegrid")
+    # Draw Feature Importances
     fig, ax = matplotlib.pyplot.subplots(figsize=(32, 18))
-    seaborn.distplot(feature_importances, kde=False, rug=True, axlabel="FeatureImportances")
-    fig.savefig(args.output[0] + ".feature_importances.png")
+    seaborn.distplot(list(filter(lambda x: x > 0, feature_importances)), hist=True, kde=False, rug=True, ax=ax)
+    matplotlib.pyplot.title("Feature Importances")
+    matplotlib.pyplot.xlabel("Feature")
+    matplotlib.pyplot.ylabel("Importances")
+    tar_files.append("importances.png")
+    fig.savefig(tar_files[-1])
     matplotlib.pyplot.close(fig)
 
-    classifier = sklearn.ensemble.RandomForestClassifier(criterion="entropy", max_features=None, n_jobs=args.cpu, random_state=0, bootstrap=False, warm_start=True)
-    for train_index, test_index in sklearn.model_selection.KFold(n_splits=5, random_state=0, shuffle=True).split(train_data):
-        classifier.fit(train_data.iloc[train_index][best_features], train_data.iloc[train_index]["Answer"])
-        print(classifier.score(train_data.iloc[test_index][best_features], train_data.iloc[test_index]["Answer"]))
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(train_data[best_features], train_data["Answer"], test_size=0.1, random_state=0, shuffle=True, stratify=train_data["Answer"])
 
-    print("Final:", classifier.score(normal_data[best_features], normal_data["Answer"]))
+    # Select best features
+    scores = [0 for _ in best_features]
+    best_num, best_score = 0, 0
+    for i in range(1, len(scores) + 1):
+        classifier.fit(x_train[best_features[:i]], y_train)
+        scores[i - 1] = tmp = classifier.score(x_test[best_features[:i]], y_test)
+        if tmp > best_score:
+            best_score = tmp
+            best_num = i
+
+    best_features = best_features[:best_num]
+    x_train = x_train[best_features]
+    x_test = x_test[best_features]
+
+    # Draw scores
+    fig, ax = matplotlib.pyplot.subplots(figsize=(32, 18))
+    matplotlib.pyplot.plot(range(1, len(scores) + 1), scores, "o-")
+    matplotlib.pyplot.xlabel("Number of Features")
+    matplotlib.pyplot.ylabel("Score")
+    matplotlib.pyplot.grid(True)
+    matplotlib.pyplot.title("Best Features: %d Features at %.2f" % (best_num, best_score))
+    tar_files.append("scores.png")
+    fig.savefig(tar_files[-1])
+    matplotlib.pyplot.close(fig)
+
+    # Draw Random Forest
+    fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
+    tree = classifier.fit(x_train, y_train)
+    sklearn.tree.plot_tree(tree.estimators_[0], ax=ax, feature_names=[list(filter(lambda x: not x.endswith("__"), x))[-1].strip() for x in list(map(lambda x: x.split(";"), best_features))], class_names=["Normal", "Premature"], filled=True)
+    matplotlib.pyplot.title("Score: %.2f" % best_score)
+    tar_files.append("tree.png")
+    fig.savefig(tar_files[-1])
+    matplotlib.pyplot.close(fig)
+
+    tar_files.append("train.tar.gz")
+    step00.make_pickle(tar_files[-1], train_data[best_features + ["Answer"]])
+
+    tar_files.append("normal.tar.gz")
+    step00.make_pickle(tar_files[-1], normal_data[best_features + ["Answer"]])
+
+    with tarfile.open(args.output[0], "w") as tar:
+        for f in tar_files:
+            tar.add(f, arcname=f)
