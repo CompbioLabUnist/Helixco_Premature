16792d16b1708d3025ddf38458fa6b3321d769e5
Jaewoong Lee <230@fumire.moe>
2020-09-23 19:51:45 +0900

    Deblur does NOT accept underscore in sample-id

--- jwlee230/Program/Python/step01-3.py
+++ jwlee230/Program/Python/step01-3.py
@@ -11,6 +11,6 @@ print("sample-id\tforward-absolute-filepath\treverse-absolute-filepath")
 for fa, fb in zip(f1, f2):
     name = fa[:-19]
     if fb.startswith(name):
-        print(name[name.rfind("/") + 1:], fa, fb, sep="\t")
+        print(name[name.rfind("/") + 1:].replace("_", "-"), fa, fb, sep="\t")
     else:
         raise ValueError(fa, fb)
