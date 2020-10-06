eb7957fc7682b9922843746a9d5986ef8061c13d
Jaewoong Lee <230@fumire.moe>
2020-10-06 20:24:36 +0900

    minor improvement

--- jwlee230/Program/Python/step26.py
+++ jwlee230/Program/Python/step26.py
@@ -106,13 +106,13 @@ if __name__ == "__main__":
 
     # Draw violin plot
     for i, feature in enumerate(best_features):
-        data = train_data[["Answer"] + [feature]]
+        data = pandas.concat([train_data, normal_data, premature_data], verify_integrity=True)[["Answer"] + [feature]]
         seaborn.set(context="poster", style="whitegrid")
         fig, ax = matplotlib.pyplot.subplots(figsize=(32, 18))
-        seaborn.violinplot(data=data, x="Answer", y=feature, ax=ax, scale="count", inner="stick")
+        seaborn.violinplot(data=data, x="Answer", y=feature, ax=ax)
         matplotlib.pyplot.xlabel("Normal/Premature")
         matplotlib.pyplot.ylabel(list(filter(lambda x: not x.endswith("__"), feature.split(";")))[-1])
-        matplotlib.pyplot.title("P-value: %.2f" % scipy.stats.ttest_ind(data.loc[(data["Answer"] == "Normal")][feature], data.loc[(data["Answer"] == "Premature")][feature], equal_var=False)[1])
+        matplotlib.pyplot.title("P-value: %.4f" % scipy.stats.ttest_ind(data.loc[(data["Answer"] == "Normal")][feature], data.loc[(data["Answer"] == "Premature")][feature], equal_var=False)[1])
         tar_files.append("feature_%d.png" % i)
         fig.savefig(tar_files[-1])
         matplotlib.pyplot.close(fig)
