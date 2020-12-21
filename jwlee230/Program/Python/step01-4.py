0e1c29d5bdf6aa5ef936f190104fa930b5f95824
Jaewoong Lee <230@fumire.moe>
2020-12-21 14:42:09 +0900

    Start with stool sample

---
+++ jwlee230/Program/Python/step01-4.py
@@ -0,0 +1,16 @@
+"""
+step01-4.py: make manifest file from stool data
+"""
+import sys
+
+f = sys.argv[1:]
+f1 = sorted(list(filter(lambda x: x.endswith("1.fastq.gz"), f)))
+f2 = sorted(list(filter(lambda x: x.endswith("2.fastq.gz"), f)))
+
+print("sample-id\tforward-absolute-filepath\treverse-absolute-filepath")
+for fa, fb in zip(f1, f2):
+    name = fa[:-19]
+    if fb.startswith(name):
+        print(name[name.rfind("/") + 1:].replace("_", "-"), fa, fb, sep="\t")
+    else:
+        raise ValueError(fa, fb)
