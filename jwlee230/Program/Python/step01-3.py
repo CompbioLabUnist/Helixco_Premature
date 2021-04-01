4f2f03c520826142f40ac6b27537f8f5e78e0a6d
Jaewoong Lee <230@fumire.moe>
2021-04-01 19:30:56 +0900

    Change the target name

--- jwlee230/Program/Python/step01-3.py
+++ jwlee230/Program/Python/step01-3.py
@@ -6,6 +6,7 @@ import sys
 f = sys.argv[1:]
 f1 = sorted(list(filter(lambda x: x.endswith("1.fastq.gz"), f)))
 f2 = sorted(list(filter(lambda x: x.endswith("2.fastq.gz"), f)))
+assert len(f1) == len(f2)
 
 print("sample-id\tforward-absolute-filepath\treverse-absolute-filepath")
 for fa, fb in zip(f1, f2):
--- jwlee230/Program/Python/step01.py
+++ jwlee230/Program/Python/step01.py
@@ -6,11 +6,12 @@ import sys
 f = sys.argv[1:]
 f1 = sorted(list(filter(lambda x: x.endswith("1.fastq.gz"), f)))
 f2 = sorted(list(filter(lambda x: x.endswith("2.fastq.gz"), f)))
+assert len(f1) == len(f2)
 
 print("sample-id\tforward-absolute-filepath\treverse-absolute-filepath")
 for fa, fb in zip(f1, f2):
     name = fa[:-11]
     if fb.startswith(name):
-        print(name[name.rfind("/") + 1:], fa, fb, sep="\t")
+        print(name[name.rfind("/") + 1:].replace("_", "-"), fa, fb, sep="\t")
     else:
         raise ValueError(fa, fb)
