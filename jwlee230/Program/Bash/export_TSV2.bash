1d6e4a09c5a97e574d5f1da4963794980e18901b
Jaewoong Lee <230@fumire.moe>
2021-08-10 19:13:43 +0900

    Add LefSe

--- jwlee230/Program/Bash/export_TSV2.bash
+++ jwlee230/Program/Bash/export_TSV2.bash
@@ -5,6 +5,7 @@ IFS=$'\n\t'
 if (($# == 2)); then
     qiime tools export --input-path $1 --output-path /tmpfs
     biom convert --input-fp /tmpfs/feature-table.biom --output-fp $2 --to-tsv
+    sed --in-place '1d' $2
 else
     echo "This script will work if and only if it has **two** arguments"
 fi
