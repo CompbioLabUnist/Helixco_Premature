b420e9061cbeaada1f0441089fd7fe89c79576ef
Jaewoong Lee <230@fumire.moe>
2021-08-10 19:17:40 +0900

    Add LefSe

---
+++ jwlee230/Program/Python/step33.py
@@ -0,0 +1,36 @@
+"""
+step33.py: Read & clearify raw TSV for LefSe
+"""
+import argparse
+import pandas
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="Input TSV file", type=str)
+    parser.add_argument("metadata", help="Metadata TSV file", type=str)
+    parser.add_argument("output", help="Output TSV file", type=str)
+    parser.add_argument("--c", help="Class used for LefSe", type=str, default="Premature")
+
+    args = parser.parse_args()
+
+    if not args.input.endswith(".tsv"):
+        raise ValueError("Input file must end with .TSV!!")
+    elif not args.metadata.endswith(".tsv"):
+        raise ValueError("Metadata file must end with .TSV!!")
+    elif not args.output.endswith(".tsv"):
+        raise ValueError("Output file must end with .TSV!!")
+
+    raw_data = pandas.read_csv(args.input, sep="\t", skiprows=1).drop(columns="#OTU ID").groupby(by="taxonomy").sum()
+    print(raw_data)
+
+    metadata = pandas.read_csv(args.metadata, sep="\t", skiprows=[1])
+    print(list(metadata.columns))
+    assert args.c in list(metadata.columns)
+    print(metadata)
+
+    raw_data.loc["Subject"] = list(metadata[args.c])
+    raw_data.sort_index(inplace=True)
+    print(raw_data)
+
+    raw_data.to_csv(args.output, sep="\t")
