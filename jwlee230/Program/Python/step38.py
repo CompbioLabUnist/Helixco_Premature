d29408952afdcd86389ad4bb032bdcac92423d85
Jaewoong Lee <230@fumire.moe>
2021-11-19 16:40:31 +0900

    Upon the issue #14

--- jwlee230/Program/Python/step38.py
+++ jwlee230/Program/Python/step38.py
@@ -5,6 +5,7 @@ import argparse
 import itertools
 import matplotlib
 import matplotlib.colors
+import matplotlib.patches
 import matplotlib.pyplot
 import seaborn
 import tqdm
@@ -15,15 +16,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("input", help="Input tar.gz file", type=str)
-    parser.add_argument("output", help="Output PDF file", type=str)
+    parser.add_argument("output", help="Output PNG file", type=str)
     parser.add_argument("--cpus", help="Number of cpus", type=int, default=1)
 
     args = parser.parse_args()
 
     if not args.input.endswith(".tar.gz"):
         raise ValueError("Input file must end with .tar.gz!!")
-    elif not args.output.endswith(".pdf"):
-        raise ValueError("Output file must end with .PDF!!")
+    elif not args.output.endswith(".png"):
+        raise ValueError("Output file must end with .PNG!!")
     elif args.cpus < 1:
         raise ValueError("CPUS must be a positive integer!!")
 
@@ -46,6 +47,8 @@ if __name__ == "__main__":
     print(colorings)
 
     g = seaborn.clustermap(data=input_data, figsize=(32, 18), row_cluster=True, col_cluster=True, row_colors=site_colors, xticklabels=False, yticklabels=False, cmap="Reds", vmin=0, vmax=1)
+
+    matplotlib.pyplot.legend([matplotlib.patches.Patch(facecolor=colorings[site]) for site in sites], sites, title="Sites", bbox_to_anchor=(1, 1), bbox_transform=matplotlib.pyplot.gcf().transFigure, loc="best")
     g.ax_heatmap.set_xlabel("Taxonomy")
     g.ax_heatmap.set_ylabel("Sample")
 
