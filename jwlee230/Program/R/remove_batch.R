adf6a7719f23d5604ec4108ac737ff4725559267
Jaewoong Lee <230@fumire.moe>
2022-02-26 16:53:58 +0900

    Add removing batch effects

---
+++ jwlee230/Program/R/remove_batch.R
@@ -0,0 +1,34 @@
+rm(list = ls())
+
+library(optparse)
+option_list = list(make_option(c("-i", "--input"), type="character", default=NULL, help="Input TSV file", metavar="character"),
+                   make_option(c("-o", "--output"), type="character", default=NULL, help="Output TSV files", metavar="character"))
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+main <- function(input, output)
+{
+    library(limma)
+    input_data <- read.csv(input, stringsAsFactors=FALSE, sep="\t", skip=1)
+
+    OTU_ID = input_data[1]
+    taxonomy_ID = input_data[length(input_data)]
+    print(head(taxonomy_ID))
+
+    input_data <- input_data[2:(length(input_data)-1)]
+    batch <- vapply(strsplit(colnames(input_data), "\\."), `[`, 1, FUN.VALUE=character(1))
+
+    output_data <- removeBatchEffect(input_data, batch=batch)
+    # output_data <- exp(output_data)
+
+    output_data <- cbind(output_data, OTU_ID, taxonomy_ID)
+
+    write.table(output_data, file=output, quote=FALSE, sep="\t", row.names=FALSE)
+}
+
+if (length(opt) == 3)
+{
+    main(opt$input, opt$output);
+} else {
+    print_help(opt_parser)
+}
