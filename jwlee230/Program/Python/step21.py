a50265cdb818d99f47316f5709b81cc99f680cc4
Jaewoong Lee <230@fumire.moe>
2022-05-18 10:44:00 +0900

    Sustain OTU ids

--- jwlee230/Program/Python/step21.py
+++ jwlee230/Program/Python/step21.py
@@ -17,12 +17,6 @@ if __name__ == "__main__":
     if not args.input.endswith(".tsv"):
         raise ValueError("Input file must end with .TSV!!")
 
-    input_data = pandas.read_csv(args.input, sep="\t", skiprows=1, index_col=0)
-    input_data["taxonomy"] = list(map(lambda x: x.replace(".", "-"), input_data["taxonomy"]))
+    input_data = pandas.read_csv(args.input, sep="\t", skiprows=1, index_col=["taxonomy", "#OTU ID"])
     print(input_data)
-
-    output_data = input_data.groupby(["taxonomy"]).sum()
-    output_data.columns = list(map(lambda x: x.replace(".", "-"), output_data.columns))
-    print(output_data)
-
-    step00.make_pickle(args.output, output_data)
+    step00.make_pickle(args.output, input_data)
