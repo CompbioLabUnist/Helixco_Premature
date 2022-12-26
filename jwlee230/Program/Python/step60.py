2957f5397a7d307ae740b3935bb4dea3ff40b90b
Jaewoong Lee <230@fumire.moe>
2022-12-26 13:52:37 +0900

    Sorted by taxa (#9)

--- jwlee230/Program/Python/step60.py
+++ jwlee230/Program/Python/step60.py
@@ -29,15 +29,14 @@ if __name__ == "__main__":
     elif not args.output.endswith(".tar"):
         raise ValueError("Output must end with .TAR!!")
 
-    input_data = pandas.read_csv(args.input, sep="\t", skiprows=1, index_col="#OTU ID").groupby("taxonomy").sum().T
+    input_data = pandas.read_csv(args.input, sep="\t", skiprows=1, index_col="#OTU ID")
+    input_data["taxonomy"] = list(map(lambda x: step00.simplified_taxonomy(x) if step00.filtering_taxonomy(x) else x, input_data["taxonomy"]))
+    input_data = input_data.groupby("taxonomy").sum().T
     print(input_data)
 
     metadata = pandas.read_csv(args.metadata, sep="\t", skiprows=[1], dtype=str, index_col="#SampleID").dropna(axis="columns", how="all")
     metadata = metadata.loc[sorted(set(input_data.index) & set(metadata.index)), :].replace(to_replace=-1, value=None).sort_values("Detail Gestational Week")
     print(metadata)
-
-    input_data = input_data.loc[metadata.index, :]
-    print(input_data)
 
     matplotlib.use("Agg")
     matplotlib.rcParams.update(step00.matplotlib_parameters)
@@ -47,9 +46,9 @@ if __name__ == "__main__":
     for s, site in step00.selected_sites_dict.items():
         tmp_data = input_data.loc[list(filter(lambda x: x.endswith(s), list(input_data.index))), :]
         taxa = sorted(list(tmp_data.columns), key=lambda x: sum(tmp_data[x]), reverse=True)
-        tmp_data = tmp_data.loc[:, taxa]
+        tmp_data = tmp_data.loc[sorted(tmp_data.index, key=lambda x: [(tmp_data.loc[x, t]) for t in taxa], reverse=True), taxa]
 
-        fig, ax = matplotlib.pyplot.subplots(figsize=(32, 18))
+        fig, axs = matplotlib.pyplot.subplots(figsize=(32, 24), nrows=2, gridspec_kw={"height_ratios": [3, 1]})
 
         for i, (color, taxon) in tqdm.tqdm(list(enumerate(zip(itertools.cycle(matplotlib.colors.XKCD_COLORS), taxa)))):
             if i < 5:
@@ -57,16 +56,26 @@ if __name__ == "__main__":
                     label = step00.simplified_taxonomy(taxon)
                 except IndexError:
                     label = step00.consistency_taxonomy(taxon, 1)
-                matplotlib.pyplot.bar(range(tmp_data.shape[0]), tmp_data.iloc[:, i], bottom=numpy.sum(tmp_data.iloc[:, :i], axis=1), color=color, linewidth=0, label=label)
+                axs[0].bar(range(tmp_data.shape[0]), tmp_data.iloc[:, i], bottom=numpy.sum(tmp_data.iloc[:, :i], axis=1), color=color, linewidth=0, label=label)
             else:
-                matplotlib.pyplot.bar(range(tmp_data.shape[0]), tmp_data.iloc[:, i], bottom=numpy.sum(tmp_data.iloc[:, :i], axis=1), color=color, linewidth=0)
+                axs[0].bar(range(tmp_data.shape[0]), tmp_data.iloc[:, i], bottom=numpy.sum(tmp_data.iloc[:, :i], axis=1), color=color, linewidth=0)
 
-        matplotlib.pyplot.xticks(range(tmp_data.shape[0]), list(map(lambda x: metadata.loc[x, "Detail Gestational Week"], list(tmp_data.index))), rotation="vertical", fontsize="xx-small")
-        matplotlib.pyplot.xlabel(f"{tmp_data.shape[0]} {site} samples (order by GW)")
-        matplotlib.pyplot.ylabel(f"{len(taxa)} bacteria")
-        matplotlib.pyplot.legend()
-        matplotlib.pyplot.grid(True)
+        axs[0].set_xticks(range(tmp_data.shape[0]), list(map(lambda x: metadata.loc[x, "Gestational Week"], list(tmp_data.index))), fontsize="xx-small", rotation="vertical")
+        axs[0].set_xlabel(f"{tmp_data.shape[0]} {site} samples")
+        axs[0].set_ylabel(f"{len(taxa)} bacteria")
+        axs[0].legend(loc="lower left")
+        axs[0].grid(True)
+
+        axs[1].bar(range(tmp_data.shape[0]), list(map(lambda x: int(metadata.loc[x, "Gestational Week"]) if (metadata.loc[x, "Detail Premature"] == "Early PTB") else 0, list(tmp_data.index))), color="tab:red", linewidth=0, label="Early PTB")
+        axs[1].bar(range(tmp_data.shape[0]), list(map(lambda x: int(metadata.loc[x, "Gestational Week"]) if (metadata.loc[x, "Detail Premature"] == "Late PTB") else 0, list(tmp_data.index))), color="tab:pink", linewidth=0, label="Late PTB")
+        axs[1].bar(range(tmp_data.shape[0]), list(map(lambda x: int(metadata.loc[x, "Gestational Week"]) if (metadata.loc[x, "Detail Premature"] == "Normal") else 0, list(tmp_data.index))), color="tab:green", linewidth=0, label="Normal")
+
         matplotlib.pyplot.tight_layout()
+        axs[1].set_xticks(range(tmp_data.shape[0]), list(map(lambda x: metadata.loc[x, "Gestational Week"], list(tmp_data.index))), fontsize="xx-small", rotation="vertical")
+        axs[1].set_xlabel(f"{tmp_data.shape[0]} {site} samples")
+        axs[1].set_ylabel("GW")
+        axs[1].legend(loc="lower left")
+        axs[1].grid(True)
 
         figures.append(f"{site}.pdf")
         fig.savefig(figures[-1])
