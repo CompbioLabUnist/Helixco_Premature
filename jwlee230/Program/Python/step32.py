8687c0ccda0f4c275d277248869d92a4eba9ba1c
Jaewoong Lee <230@fumire.moe>
2023-01-12 18:59:26 +0900

    Update prescale with sites (#19)

--- jwlee230/Program/Python/step32.py
+++ jwlee230/Program/Python/step32.py
@@ -6,6 +6,7 @@ import itertools
 import multiprocessing
 import tarfile
 import typing
+import warnings
 import matplotlib
 import matplotlib.colors
 import matplotlib.pyplot
@@ -24,43 +25,54 @@ metadata = pandas.DataFrame()
 
 
 def draw(metric: str, column: str, site: str) -> str:
-    selected_distance_data = distance_data[metric].loc[(metadata["Site"] == site), (metadata["Site"] == site)]
-    selected_tsne_data = tsne_data[metric].loc[(tsne_data[metric]["Site"] == site)]
+    selected_distance_data = distance_data[metric].loc[(metadata["Site"] == site), (metadata["Site"] == site)].copy()
+    selected_tsne_data = tsne_data[metric].loc[(tsne_data[metric]["Site"] == site)].copy(deep=True)
+
+    for c in ["tSNE1", "tSNE2"]:
+        selected_tsne_data[c] = sklearn.preprocessing.scale(selected_tsne_data[c])
 
     palette = dict(zip(sorted(set(selected_tsne_data[column])), matplotlib.colors.XKCD_COLORS))
 
-    fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
-    seaborn.scatterplot(data=selected_tsne_data, x="tSNE1", y="tSNE2", ax=ax, hue=column, hue_order=sorted(set(selected_tsne_data[column])), s=40 ** 2, palette=palette)
-
-    for item, color in palette.items():
-        step00.confidence_ellipse(selected_tsne_data.loc[(selected_tsne_data[column] == item), "tSNE1"], selected_tsne_data.loc[(selected_tsne_data[column] == item), "tSNE2"], ax, facecolor=color, alpha=0.3)
-
     try:
-        p_value = skbio.stats.distance.permanova(skbio.stats.distance.DistanceMatrix(selected_distance_data), list(selected_tsne_data[column]), permutations=step00.small)["p-value"]
+        fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
+        seaborn.scatterplot(data=selected_tsne_data, x="tSNE1", y="tSNE2", ax=ax, hue=column, hue_order=sorted(set(selected_tsne_data[column])), s=40 ** 2, palette=palette)
+
+        for item, color in palette.items():
+            step00.confidence_ellipse(selected_tsne_data.loc[(selected_tsne_data[column] == item), "tSNE1"], selected_tsne_data.loc[(selected_tsne_data[column] == item), "tSNE2"], ax, facecolor=color, alpha=0.3)
+
+        try:
+            p_value = skbio.stats.distance.permanova(skbio.stats.distance.DistanceMatrix(selected_distance_data), list(selected_tsne_data[column]), permutations=step00.small)["p-value"]
+        except ValueError:
+            p_value = 1.0
+
+        matplotlib.pyplot.title("{0} (p={1:.3f})".format(column, p_value))
+        matplotlib.pyplot.tight_layout()
+
+        file_name = "{2}+{0}+{1}.pdf".format(column.replace(" ", "_"), metric, site)
+        fig.savefig(file_name)
+        matplotlib.pyplot.close(fig)
+        return file_name
     except ValueError:
-        p_value = 1.0
-
-    matplotlib.pyplot.title("{0} (p={1:.3f})".format(column, p_value))
-    matplotlib.pyplot.tight_layout()
-
-    file_name = "{2}+{0}+{1}.pdf".format(column.replace(" ", "_"), metric, site)
-    fig.savefig(file_name)
-    matplotlib.pyplot.close(fig)
-    return file_name
+        return ""
 
 
 def draw_all(metric: str, column: str) -> str:
-    palette = dict(zip(sorted(set(tsne_data[metric][column])), matplotlib.colors.XKCD_COLORS))
+    data = tsne_data[metric].copy()
+
+    for c in ["tSNE1", "tSNE2"]:
+        data[c] = sklearn.preprocessing.scale(data[c])
+
+    palette = dict(zip(sorted(set(data[column])), matplotlib.colors.XKCD_COLORS))
 
     fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
 
-    seaborn.scatterplot(data=tsne_data[metric], x="tSNE1", y="tSNE2", ax=ax, hue=column, hue_order=sorted(set(tsne_data[metric][column])), s=40 ** 2, palette=palette)
+    seaborn.scatterplot(data=data, x="tSNE1", y="tSNE2", ax=ax, hue=column, hue_order=sorted(set(tsne_data[metric][column])), s=40 ** 2, palette=palette)
 
     for item, color in palette.items():
-        step00.confidence_ellipse(tsne_data[metric].loc[(tsne_data[metric][column] == item), "tSNE1"], tsne_data[metric].loc[(tsne_data[metric][column] == item), "tSNE2"], ax, facecolor=color, alpha=0.3)
+        step00.confidence_ellipse(data.loc[(data[column] == item), "tSNE1"], data.loc[(data[column] == item), "tSNE2"], ax, facecolor=color, alpha=0.3)
 
     try:
-        p_value = skbio.stats.distance.permanova(skbio.stats.distance.DistanceMatrix(distance_data[metric]), list(tsne_data[metric][column]), permutations=step00.small)["p-value"]
+        p_value = skbio.stats.distance.permanova(skbio.stats.distance.DistanceMatrix(distance_data[metric]), list(data[column]), permutations=step00.small)["p-value"]
     except ValueError:
         p_value = 1.0
 
@@ -74,6 +86,8 @@ def draw_all(metric: str, column: str) -> str:
 
 
 if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument("input", type=str, help="Input TSV file")
@@ -103,7 +117,7 @@ if __name__ == "__main__":
 
     metadata = pandas.read_csv(args.metadata, sep="\t", skiprows=[1], dtype=str).dropna(axis="columns", how="all").set_index(keys=["#SampleID"], verify_integrity=True)
     metadata = metadata.loc[sorted(set(input_data.index) & set(metadata.index)), sorted(set(metadata.columns) - step00.numeric_columns)].replace(to_replace=-1, value=None)
-    columns = set(metadata.columns) - step00.numeric_columns - {"Mother", "Neonate"}
+    columns = set(metadata.columns) - step00.numeric_columns - {"Mother", "Neonate", "Gestational Week", "Detail Gestational Week"}
     print(metadata)
     print(sorted(columns))
 
@@ -115,9 +129,6 @@ if __name__ == "__main__":
 
         tsne_data[metric] = pandas.DataFrame(sklearn.manifold.TSNE(n_components=2, init="pca", random_state=0, method="exact", n_jobs=args.cpus, perplexity=50).fit_transform(distance_data[metric]), columns=["tSNE1", "tSNE2"])
 
-        for column in list(tsne_data[metric].columns):
-            tsne_data[metric][column] = sklearn.preprocessing.scale(tsne_data[metric][column])
-
         tsne_data[metric]["index"] = list(distance_data[metric].index)
         tsne_data[metric].set_index(keys="index", inplace=True, verify_integrity=True)
 
@@ -128,6 +139,8 @@ if __name__ == "__main__":
     with multiprocessing.Pool(args.cpus) as pool:
         files += pool.starmap(draw, itertools.product(distance_data.keys(), columns, step00.selected_long_sites))
         files += pool.starmap(draw_all, itertools.product(distance_data.keys(), columns))
+
+    files = list(filter(None, files))
 
     with tarfile.open(args.output, "w") as tar:
         for f in tqdm.tqdm(files):
